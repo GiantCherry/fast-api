5a810e39d8efa2e2a0756f9baa99ab7cef17397f
GiantCherry <lunev1981@mail.ru>
2025-07-03 21:31:23 +0300

    Adds two numbers. Post query

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 async def root():
     return FileResponse('fast-api/index.html')
+
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+
+@app.post('/calculate')
+async def calculate(numbers: Numbers):
+    result = numbers.num1 + numbers.num2
+    return {'result' : result}
