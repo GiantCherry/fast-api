101a7d8390730fb6840cbc0ea0343217ca41b33e
GiantCherry <lunev1981@mail.ru>
2025-07-06 22:12:50 +0300

    Tuning project's features

--- app/main.py
+++ app/main.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI
 
+from app.config import load_config
+from app.logger import logger
+
+
+# import logging
+
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+config = load_config()
+
+# app = FastAPI(debug=config.debug)  # Устанавливаем debug режим при создании
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {'message' : 'Hello, World!'}
+if config.debug:
+    app.debug = True
+else:
+    app.debug = False
 
-@app.get('/custom')
+
+@app.get("/")
+def read_root():
+    logger.info("Handlig request to root endpoint")
+    return {"message": "Hello, World!"}
+
+
+@app.get("/custom")
 def read_custom_message():
-    return {'message' : 'This is a custom message!'}
+    return {"message": "This is a custom message!"}
+
+
+@app.get("/db")
+def get_db_info():
+    logger.info(f"Connecting to database: {config.db.database_url}")
+    return {"database_url": config.db.database_url}
